8d8e9d47eb85a8f54eaae67bef7483f0490764f7
moonuel <co.manuel@hotmail.com>
2026-05-06 13:36:17 -0400

    feat(scripts): create view of census table with only selected features

--- scripts/create_census_2021.py
+++ scripts/create_census_2021.py
@@ -5,7 +5,7 @@ print(duckdb.query("SELECT * FROM read_csv('data/statcan census 2021/98-401-X202
 
 con = duckdb.connect("data/migration_datasets.duckdb")
 
-con.query("CREATE VIEW census_2021 AS " \
+con.query("CREATE TABLE census_2021 AS " \
 "SELECT * FROM read_csv('data/statcan census 2021/98-401-X2021006_English_CSV_data_Ontario.csv', encoding='latin-1')")
 
 con.close()
