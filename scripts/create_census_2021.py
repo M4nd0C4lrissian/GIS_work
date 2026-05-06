3fc87a5febacea107984a53a13fc4b03351799f1
moonuel <co.manuel@hotmail.com>
2026-05-06 12:09:53 -0400

    create: add census 2021 data ingestion script

---
+++ scripts/create_census_2021.py
@@ -0,0 +1,11 @@
+import duckdb
+import pandas as pd
+
+print(duckdb.query("SELECT * FROM read_csv('data/statcan census 2021/98-401-X2021006_English_CSV_data_Ontario.csv', encoding='latin-1')"))
+
+con = duckdb.connect("data/migration_datasets.duckdb")
+
+con.query("CREATE VIEW census_2021 AS " \
+"SELECT * FROM read_csv('data/statcan census 2021/98-401-X2021006_English_CSV_data_Ontario.csv', encoding='latin-1')")
+
+con.close()
